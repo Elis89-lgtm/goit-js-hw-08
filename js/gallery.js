5b15ab8e749963e6e4fa5bce9c793ef89532dc33
Elizabeth Dzhuravel <eliz5678@gmail.com>
2025-01-26 23:36:22 +0200

    add styles.css index.html gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -1,111 +1,84 @@
 
 const images = [
-   {
-   preview:
-   'https://cdn.pixabay.com/photo/ 2019/05/14/16/43 /rchids- 420282 0__480.jpg',
-   original:
-   'https://cdn.pixabay.com/photo/ 2019/05/14/16/43 /rchids- 420282 0_ 1280 .jpg',
-  описание: 'Hokkaido Flower',
-   },
   {
-   preview:
-   'https://cdn.pixabay.com/photo/ 2019/05/14/22/05 /container- 420367 7__340.jpg',
-   original:
-   'https://cdn.pixabay.com/photo/ 2019/05/14/22/05 /container- 420367 7_ 1280 .jpg',
- описание: 'Container Haulage Freight',
- },
-   {
-   preview:
-   'https://cdn.pixabay.com/photo/ 2019/05/16/09/47 /beach- 420678 5__340.jpg',
-  original:
-   'https://cdn.pixabay.com/photo/ 2019/05/16/09/47 /beach- 420678 5_ 1280 .jpg',
-   описание: 'Aerial Beach View',
-   },
-   {
-   preview:
-   'https://cdn.pixabay.com/photo/ 2016/11/18/16/19 /flowers- 183561 9__340.jpg',
-   original:
-   'https://cdn.pixabay.com/photo/ 2016/11/18/16/19 /flowers- 183561 9_ 1280 .jpg',
-  описание: 'Flower Blooms',
-   },
-   {
- preview:
-   'https://cdn.pixabay.com/photo/ 2018/09/13/10/36 /mountains- 367433 4__340.jpg',
-   original:
-  'https://cdn.pixabay.com/photo/ 2018/09/13/10/36 /mountains- 367433 4_ 1280 .jpg',
-   описание: 'Alpine Mountains',
-   },
-  {
-   preview:
-   'https://cdn.pixabay.com/photo/ 2019/05/16/23/04 /landscape- 420857 1__340.jpg',
-   original:
-   'https://cdn.pixabay.com/photo/ 2019/05/16/23/04 /landscape- 420857 1_ 1280 .jpg',
-   описание: 'Mountain Lake Sailing',
+    preview: 'https://cdn.pixabay.com/photo/2019/05/14/16/43/orchids-4202820__480.jpg',
+    original: 'https://cdn.pixabay.com/photo/2019/05/14/16/43/orchids-4202820_1280.jpg',
+    description: 'Hokkaido Flower',
   },
-   {
-   preview:
-   'https://cdn.pixabay.com/photo/ 2019/05/17/09/27 /the-alps- 420927 2__340.jpg',
-   original:
-   'https://cdn.pixabay.com/photo/ 2019/05/17/09/27 /the-alps- 420927 2_ 1280 .jpg',
-   описание: 'Alpine Spring Meadows',
-   },
-   {
-   preview:
-   'https://cdn.pixabay.com/photo/ 2019/05/16/21/10 /landscape- 420825 5__340.jpg',
-   original:
-  'https://cdn.pixabay.com/photo/ 2019/05/16/21/10 /landscape- 420825 5_ 1280 .jpg',
- описание: 'Nature Landscape',
-   },
-   {
-   preview:
-   'https://cdn.pixabay.com/photo/ 2019/05/17/04/35 /lighthouse- 420884 3__340.jpg',
-   original:
-   'https://cdn.pixabay.com/photo/ 2019/05/17/04/35 /lighthouse- 420884 3_ 1280 .jpg',
-   описание: 'Lighthouse Coast Sea',
-   },
-  ];
+  {
+    preview: 'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
+    original: 'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
+    description: 'Container Haulage Freight',
+  },
+  {
+    preview: 'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
+    original: 'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
+    description: 'Aerial Beach View',
+  },
+  {
+    preview: 'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
+    original: 'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
+    description: 'Flower Blooms',
+  },
+  {
+    preview: 'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
+    original: 'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
+    description: 'Alpine Mountains',
+  },
+  {
+    preview: 'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
+    original: 'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
+    description: 'Mountain Lake Sailing',
+  },
+  {
+    preview: 'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
+    original: 'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
+    description: 'Alpine Spring Meadows',
+  },
+  {
+    preview: 'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
+    original: 'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
+    description: 'Nature Landscape',
+  },
+  {
+    preview: 'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
+    original: 'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
+    description: 'Lighthouse Coast Sea',
+  },
+];
+
+  
 //Используем метод map для создания разметки галереи и добавляем ее в <ul>
   const galleryContainer = document.querySelector('.gallery');
 
 const galleryMarkup = images
   .map(
     ({ preview, original, description }) => `
-      <li class="gallery-item">
-        <a class="gallery-link" href="large-image.jpg" >
-          <img
-            class = "gallery-image" 
-            src = "small-image.jpg" 
-            data-source = "large-image.jpg" 
-            alt = "Image description"
-          />
-        </a>
-      </li>`
+        <li class="gallery-item">
+          <a class="gallery-link" href="${original}">
+            <img
+              class="gallery-image"
+              src="${preview}"
+              data-source="${original}"
+              alt="${description}"
+            />
+          </a>
+        </li>`
   )
   .join('');
-//Добавляем обработчик кликов с делегированием:
+  galleryContainer.innerHTML = galleryMarkup;
 
+galleryContainer.addEventListener('click', (e) => {
+  e.preventDefault();
 
-galleryContainer.innerHTML = galleryMarkup;
-
-galleryContainer.addEventListener('click', (event) => {
-  event.preventDefault();
-
-  if (event.target.nodeName !== 'IMG') {
+  if (e.target.nodeName !== 'IMG') {
     return;
   }
 
-  console.log(event.target.dataset.source);
-});
+  
 
-//Добавляем код для открытия модального окна с библиотекой:
-galleryContainer.addEventListener('click', (event) => {
-  event.preventDefault();
+const imageSource = e.target.dataset.source;
 
-  if (event.target.nodeName !== 'IMG') {
-    return;
-  }
-
-  const imageSource = event.target.dataset.source;
 
   const instance = basicLightbox.create(`
     <img src="${imageSource}" width="800" height="600">
